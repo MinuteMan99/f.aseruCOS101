61b8c992c14781d7be2170b8cca0e643fb3cfbcc
Folarin Aseru <folarin.aseru@pau.edu.ng>
2025-11-26 12:35:10 +0100

    The practices on the methods of file structs

---
+++ week-9/practice_1/target/debug/deps/practice_1.d
@@ -0,0 +1,5 @@
+C:\Users\Folarin Aseru\Desktop\PAU\COS 101\f.aseruCOS101\week-9\practice_1\target\debug\deps\practice_1.d: src\main.rs
+
+C:\Users\Folarin Aseru\Desktop\PAU\COS 101\f.aseruCOS101\week-9\practice_1\target\debug\deps\practice_1.exe: src\main.rs
+
+src\main.rs:
---
+++ week-9/practice_2/target/debug/practice_2.d
@@ -0,0 +1,1 @@
+C:\Users\Folarin\ Aseru\Desktop\PAU\COS\ 101\f.aseruCOS101\week-9\practice_2\target\debug\practice_2.exe: C:\Users\Folarin\ Aseru\Desktop\PAU\COS\ 101\f.aseruCOS101\week-9\practice_2\src\main.rs

293aa74b9cb40d1600af03a119701af135eb3a7f
Folarin Aseru <folarin.aseru@pau.edu.ng>
2025-12-06 13:15:18 +0100

    Passing a struct to a function

---
+++ week-10/practice_8/target/debug/deps/practice_8.d
@@ -0,0 +1,5 @@
+C:\Users\Folarin Aseru\Desktop\PAU\COS 101\f.aseruCOS101\week-10\practice_8\target\debug\deps\practice_8.d: src\main.rs
+
+C:\Users\Folarin Aseru\Desktop\PAU\COS 101\f.aseruCOS101\week-10\practice_8\target\debug\deps\practice_8.exe: src\main.rs
+
+src\main.rs:
